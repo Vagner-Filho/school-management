0c3d0c15bc3cfcdd720d35110bdbcf51be0a561c
Vagner Filho <vagner.martins@sesims.com.br>
2021-12-21 17:28:47 -0400

    Connection and school model

---
+++ Model/Aluno.java
@@ -0,0 +1,8 @@
+public class Aluno {
+  public int aluno_id;
+  public String aluno_nome;
+  public String aluno_cpf;
+  public String aluno_matricula;
+  public String aluno_email;
+  public Escola escola;
+}
