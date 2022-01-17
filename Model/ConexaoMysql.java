d11a0a3abf8a80bde8fcdd7306a25ead2fa56072
Vagner Simone Martins Filho <vagner.martins@sesims.com.br>
2022-01-16 23:17:15 -0300

    connector j

--- Model/ConexaoMysql.java
+++ Model/ConexaoMysql.java
@@ -1,15 +1,17 @@
-
-import java.sql.*;
+// import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConexaoMysql {
   private static Connection conexao;
 
   public static Connection conectar() {
-    String nomeDriver = "com.mysql.jdbc.Driver";
-    String usuario = "root", senha = "";
+    // String nomeDriver = "com.mysql.jdbc.Driver";
+    String usuario = "root", senha = "luaazul69";
 
     try {
-      Class.forName(nomeDriver);
+      // Class.forName(nomeDriver);
 
       conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/escola?user=" + usuario + "&password=" + senha);
       System.out.println("Conexão efetuada!");
